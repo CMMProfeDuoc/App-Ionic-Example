f957ae45bcc3c1066b5710427497882d2b8b883e
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-11-04 10:24:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EjemploClases',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
